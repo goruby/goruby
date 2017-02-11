7c2080949d8326599989730101418bcab4f5020c
Michael Wagner <mitch.wagna@gmail.com>
2017-02-11 14:12:52 +0100

    feat(ast): add expression statement

--- ast/ast.go
+++ ast/ast.go
@@ -50,3 +50,11 @@ type ReturnStatement struct {
 
 func (rs *ReturnStatement) statementNode()       {}
 func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+
+type ExpressionStatement struct {
+	Token      token.Token // the first token of the expression
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode()       {}
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
