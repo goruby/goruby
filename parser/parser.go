38f0b48238fb633d0d4817906ef1ccee316bbe7e
Michael Wagner <mitch.wagna@gmail.com>
2017-02-11 22:12:21 +0100

    feat(parser): add grouped expressions

--- parser/parser.go
+++ parser/parser.go
@@ -52,6 +52,7 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerPrefix(token.MINUS, p.parsePrefixExpression)
 	p.registerPrefix(token.TRUE, p.parseBoolean)
 	p.registerPrefix(token.FALSE, p.parseBoolean)
+	p.registerPrefix(token.LPAREN, p.parseGroupedExpression)
 
 	p.infixParseFns = make(map[token.TokenType]infixParseFn)
 	p.registerInfix(token.PLUS, p.parseInfixExpression)
@@ -229,6 +230,15 @@ func (p *Parser) parseInfixExpression(left ast.Expression) ast.Expression {
 	p.nextToken()
 	expression.Right = p.parseExpression(precedence)
 	return expression
+}
+
+func (p *Parser) parseGroupedExpression() ast.Expression {
+	p.nextToken()
+	exp := p.parseExpression(LOWEST)
+	if !p.accept(token.RPAREN) {
+		return nil
+	}
+	return exp
 }
 
 func (p *Parser) peekPrecedence() int {
