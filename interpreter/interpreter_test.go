ea5625e80e165dc6447b8cd46fc4623338439291
Michael Wagner <mitch.wagna@gmail.com>
2017-04-19 16:24:13 +0200

    fix: retain self after class definition

--- interpreter/interpreter_test.go
+++ interpreter/interpreter_test.go
@@ -127,3 +127,65 @@ func TestModuleInEnv(t *testing.T) {
 		t.Fail()
 	}
 }
+
+func TestInterpretClasses(t *testing.T) {
+	t.Run("evaluate class object", func(t *testing.T) {
+		input := `
+		class Foo
+			def foo
+				3
+			end
+		end
+		Foo
+		`
+
+		i := New()
+
+		evaluated, err := i.Interpret(input)
+		if err != nil {
+			t.Logf("Expected no error, got %T:%v", err, err)
+			t.FailNow()
+		}
+
+		classObject, ok := evaluated.(object.RubyClassObject)
+		if !ok {
+			t.Logf("Expected class object, got %T", evaluated)
+			t.FailNow()
+		}
+
+		_, ok = classObject.Methods().Get("foo")
+		if !ok {
+			t.Logf("Expected class object to have method foo")
+			t.Fail()
+		}
+	})
+	t.Run("self after class definition", func(t *testing.T) {
+		input := `
+		class Foo
+			def foo
+				3
+			end
+		end
+		self
+		`
+
+		i := New()
+
+		evaluated, err := i.Interpret(input)
+		if err != nil {
+			t.Logf("Expected no error, got %T:%v", err, err)
+			t.FailNow()
+		}
+
+		self, ok := evaluated.(*object.Self)
+		if !ok {
+			t.Logf("Expected self, got %T", evaluated)
+			t.FailNow()
+		}
+
+		if self.Name != "main" {
+			t.Logf("Expected main object, got %s", self.Name)
+			t.Fail()
+		}
+	})
+}
