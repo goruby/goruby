2cfee39c6cf900583fd92eb65702755136d9f755
Michael Wagner <mitch.wagna@gmail.com>
2017-05-15 18:44:02 +0200

    feat(object): add Array.push

---
+++ object/array_test.go
@@ -0,0 +1,51 @@
+package object
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestArrayPush(t *testing.T) {
+	t.Run("one argument", func(t *testing.T) {
+		array := &Array{}
+		env := NewEnvironment()
+		context := &callContext{
+			receiver: array,
+			env:      env,
+		}
+
+		result, err := arrayPush(context, &Integer{Value: 17})
+
+		checkError(t, err, nil)
+
+		expectedResult := &Array{Elements: []RubyObject{&Integer{Value: 17}}}
+
+		checkResult(t, result, expectedResult)
+
+		if !reflect.DeepEqual(expectedResult, array) {
+			t.Logf("Expected array to equal\n%+#v\n\tgot\n%+#v\n", expectedResult, array)
+			t.Fail()
+		}
+	})
+	t.Run("more than one argument", func(t *testing.T) {
+		array := &Array{}
+		env := NewEnvironment()
+		context := &callContext{
+			receiver: array,
+			env:      env,
+		}
+
+		result, err := arrayPush(context, &Integer{Value: 17}, NIL, TRUE, FALSE)
+
+		checkError(t, err, nil)
+
+		expectedResult := &Array{Elements: []RubyObject{&Integer{Value: 17}, NIL, TRUE, FALSE}}
+
+		checkResult(t, result, expectedResult)
+
+		if !reflect.DeepEqual(expectedResult, array) {
+			t.Logf("Expected array to equal\n%+#v\n\tgot\n%+#v\n", expectedResult, array)
+			t.Fail()
+		}
+	})
+}
