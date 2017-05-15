6a22d808dd1f6d127fbe6e409fe3f4ae8d480726
Michael Wagner <mitch.wagna@gmail.com>
2017-05-15 18:50:19 +0200

    feat(object): add Kernel.tap
    This showcases the usage of blocks within core methods

--- interpreter/method_calls_test.go
+++ interpreter/method_calls_test.go
@@ -131,3 +131,25 @@ func TestMethodBlockLeakage(t *testing.T) {
 		}
 	})
 }
+
+func TestKernelTap(t *testing.T) {
+	input := `
+		x = []
+		x.tap {|z|z.push(true)}
+	`
+	i := New()
+
+	evaluated, err := i.Interpret(input)
+	if err != nil {
+		t.Logf("Expected no error, got %T:%v", err, err)
+		t.FailNow()
+	}
+
+	expected := &object.Array{Elements: []object.RubyObject{object.TRUE}}
+	actual := evaluated
+
+	if !reflect.DeepEqual(expected, actual) {
+		t.Logf("Expected result to equal\n%+#v\n\tgot\n%+#v\n", expected, actual)
+		t.Fail()
+	}
+}
