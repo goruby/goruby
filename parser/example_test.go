09207c55fc8610a1f9727da89a25cd4b0e451ab6
Michael Wagner <mitch.wagna@gmail.com>
2017-10-06 20:43:56 +0200

    refactor(parser): refine parse interface

---
+++ parser/example_test.go
@@ -0,0 +1,44 @@
+package parser_test
+
+import (
+	"fmt"
+
+	"go/token"
+
+	"github.com/goruby/goruby/ast"
+	"github.com/goruby/goruby/parser"
+)
+
+func ExampleParseFile() {
+	fset := token.NewFileSet() // positions are relative to fset
+
+	src := `LANG = "Ruby"
+
+module Foo
+
+	def bar()
+		puts "Hello world"
+	end
+
+end`
+
+	f, err := parser.ParseFile(fset, "", src)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Print the statements from the programs AST
+	for _, s := range f.Statements {
+		if exp, ok := s.(*ast.ExpressionStatement); ok {
+			fmt.Printf("%T\n", exp.Expression)
+		} else {
+			fmt.Printf("%T\n", s)
+		}
+	}
+
+	// output:
+	//
+	// *ast.VariableAssignment
+	// *ast.ModuleExpression
+}
