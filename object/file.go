f56e02d4030667f211fc4f5df4654db16a070e17
Michael Wagner <mitch.wagna@gmail.com>
2017-10-10 16:38:14 +0200

    feat(object): add `File`
    Already included methods: `expand_path`

---
+++ object/file.go
@@ -0,0 +1,75 @@
+package object
+
+import (
+	"path/filepath"
+)
+
+var fileClass RubyClassObject = newClass(
+	"File",
+	objectClass,
+	fileMethods,
+	fileClassMethods,
+	func(RubyClassObject) RubyObject { return &File{make(map[RubyObject]RubyObject)} },
+)
+
+func init() {
+	classes.Set("File", fileClass)
+}
+
+// A File represents the Ruby class File
+type File struct {
+	Map map[RubyObject]RubyObject
+}
+
+// Type returns the ObjectType of the array
+func (f *File) Type() Type { return OBJECT_OBJ }
+
+// Inspect returns all elements within the array, divided by comma and
+// surrounded by brackets
+func (f *File) Inspect() string {
+	return ""
+}
+
+// Class returns the class of the Array
+func (f *File) Class() RubyClass { return fileClass }
+
+var fileClassMethods = map[string]RubyMethod{
+	"expand_path": publicMethod(fileExpandPath),
+}
+
+var fileMethods = map[string]RubyMethod{}
+
+func fileExpandPath(context CallContext, args ...RubyObject) (RubyObject, error) {
+	switch len(args) {
+	case 1:
+		str, ok := args[0].(*String)
+		if !ok {
+			return nil, NewImplicitConversionTypeError(str, args[0])
+		}
+		path, err := filepath.Abs(str.Value)
+
+		if err == nil {
+			return &String{Value: path}, nil
+		}
+
+		return nil, NewNotImplementedError("Cannot determine working directory")
+	case 2:
+		filename, ok := args[0].(*String)
+		if !ok {
+			return nil, NewImplicitConversionTypeError(filename, args[0])
+		}
+		dirname, ok := args[1].(*String)
+		if !ok {
+			return nil, NewImplicitConversionTypeError(filename, args[0])
+		}
+		// TODO: make sure this is really the wanted behaviour
+		abs, err := filepath.Abs(filepath.Join(dirname.Value, filename.Value))
+		if err != nil {
+			return nil, NewNotImplementedError(err.Error())
+		}
+
+		return &String{Value: abs}, nil
+	default:
+		return nil, NewWrongNumberOfArgumentsError(1, len(args))
+	}
+}
---
+++ object/file_test.go
@@ -0,0 +1,50 @@
+package object
+
+import (
+	"os"
+	"testing"
+)
+
+func TestFileExpandPath(t *testing.T) {
+	t.Run("one arg flavour", func(t *testing.T) {
+		env := NewEnvironment()
+		context := &callContext{
+			receiver: &Self{RubyObject: fileClass, Name: "File"},
+			env:      env,
+		}
+		filename := &String{Value: "./fixtures/testfile.rb"}
+
+		result, err := fileExpandPath(context, filename)
+
+		checkError(t, err, nil)
+
+		cwd, err := os.Getwd()
+		if err != nil {
+			t.Skip("Cannot determine working directory")
+		}
+		expected := &String{Value: cwd + "/fixtures/testfile.rb"}
+
+		checkResult(t, result, expected)
+	})
+	t.Run("two arg flavour", func(t *testing.T) {
+		env := NewEnvironment()
+		context := &callContext{
+			receiver: &Self{RubyObject: fileClass, Name: "File"},
+			env:      env,
+		}
+		filename := &String{Value: "../../main.go"}
+		dirname := &String{Value: "object/fixtures/"}
+
+		result, err := fileExpandPath(context, filename, dirname)
+
+		checkError(t, err, nil)
+
+		cwd, err := os.Getwd()
+		if err != nil {
+			t.Skip("Cannot determine working directory")
+		}
+		expected := &String{Value: cwd + "/main.go"}
+
+		checkResult(t, result, expected)
+	})
+}
